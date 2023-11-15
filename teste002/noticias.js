2a32edd71114169c33716a6a1f94c8fa6f243651
Sérgio Henrique <shbarros3@gmail.com>
2023-11-15 00:55:34 -0300

    atualizei

---
+++ teste002/noticias.js
@@ -0,0 +1,33 @@
+//link externo das noticias
+
+let noticias = document.querySelector('.noticia-destaque-single');
+
+noticias.addEventListener('click', ()=>{
+    window.location.href="https://noticias.r7.com/economia/energia-solar-e-a-segunda-mais-usada-no-brasil-mostra-associacao-17082023"
+})
+
+//noticias secundarias
+
+let noticiasS2 = document.querySelector('.outras-noticias-1-single');
+
+noticiasS2.addEventListener('click', ()=>{
+    window.location.href="https://www.gov.br/mdic/pt-br/assuntos/noticias/2023/novembro/alckmin-inaugura-megausina-de-energia-solar-em-mg"
+})
+
+let noticiasS3 = document.querySelector('.outras-noticias-2-single');
+
+noticiasS3.addEventListener('click', ()=>{
+    window.location.href="https://www.portalsolar.com.br/noticias/mercado/internacional/brasil-sera-quinto-maior-mercado-de-energia-solar-do-mundo-ate-2032"
+})
+
+let noticiasS4 = document.querySelector('.outras-noticias-3-single');
+
+noticiasS4.addEventListener('click', ()=>{
+    window.location.href="https://g1.globo.com/jornal-nacional/noticia/2023/07/07/capacidade-de-producao-da-energia-solar-no-primeiro-semestre-de-2023-supera-todo-o-resultado-de-2022.ghtml"
+})
+
+let noticiasS5 = document.querySelector('.outras-noticias-4-single');
+
+noticiasS5.addEventListener('click', ()=>{
+    window.location.href="https://www.terra.com.br/noticias/geracao-de-energia-solar-pode-movimentar-r-22-tri-ate-2050,f255abdf4dec60a430ad8f5fb62187c18qv0an7h.html"
+})
