35ead73ee96c81c40f24df2e6d3e9e08d2216b76
Sérgio Henrique <shbarros3@gmail.com>
2023-11-17 23:53:43 -0300

    atualizei

--- teste002/javascript/noticias.js
+++ teste002/javascript/noticias.js
@@ -1,61 +1,7 @@
 //link externo das noticias
 
-let noticias = document.querySelector('.noticia-destaque-single');
+let noticias = document.querySelector('.1');
 
 noticias.addEventListener('click', ()=>{
-    window.location.href="https://noticias.r7.com/economia/energia-solar-e-a-segunda-mais-usada-no-brasil-mostra-associacao-17082023"
-})
-
-let noticiasS2 = document.querySelector('.outras-noticias-1-single');
-
-noticiasS2.addEventListener('click', ()=>{
-    window.location.href="https://www.gov.br/mdic/pt-br/assuntos/noticias/2023/novembro/alckmin-inaugura-megausina-de-energia-solar-em-mg"
-})
-
-let noticiasS3 = document.querySelector('.outras-noticias-2-single');
-
-noticiasS3.addEventListener('click', ()=>{
-    window.location.href="https://www.portalsolar.com.br/noticias/mercado/internacional/brasil-sera-quinto-maior-mercado-de-energia-solar-do-mundo-ate-2032"
-})
-
-let noticiasS4 = document.querySelector('.outras-noticias-3-single');
-
-noticiasS4.addEventListener('click', ()=>{
-    window.location.href="https://g1.globo.com/jornal-nacional/noticia/2023/07/07/capacidade-de-producao-da-energia-solar-no-primeiro-semestre-de-2023-supera-todo-o-resultado-de-2022.ghtml"
-})
-
-let noticiasS5 = document.querySelector('.outras-noticias-4-single');
-
-noticiasS5.addEventListener('click', ()=>{
-    window.location.href="https://www.terra.com.br/noticias/geracao-de-energia-solar-pode-movimentar-r-22-tri-ate-2050,f255abdf4dec60a430ad8f5fb62187c18qv0an7h.html"
-})
-
-let noticiasS6 = document.querySelector('.noticia-destaque-2-single');
-
-noticiasS6.addEventListener('click', ()=>{
     window.location.href="https://epbr.com.br/amazon-anuncia-primeiro-parque-eolico-no-brasil/"
-})
-
-let noticiasS7 = document.querySelector('.outras-noticias-5-single');
-
-noticiasS7.addEventListener('click', ()=>{
-    window.location.href="https://umsoplaneta.globo.com/energia/noticia/2023/05/11/pela-1a-vez-na-historia-energia-eolica-supera-gas-e-e-a-principal-fonte-de-eletricidade-no-reino-unido.ghtml"
-})
-
-let noticiasS8 = document.querySelector('.outras-noticias-6-single');
-
-noticiasS8.addEventListener('click', ()=>{
-    window.location.href="https://www.infomoney.com.br/negocios/empresas-miram-transicao-energetica-e-investem-bilhoes-de-reais-em-usinas-eolicas-no-brasil/"
-})
-
-let noticiasS9 = document.querySelector('.outras-noticias-7-single');
-
-noticiasS9.addEventListener('click', ()=>{
-    window.location.href="https://www.agenciaminas.mg.gov.br/noticia/parque-eolico-volta-do-rio-passa-a-operar-com-totalidade-sob-gestao-da-cemig"
-})
-
-let noticiasS10 = document.querySelector('.outras-noticias-7-single');
-
-noticiasS10.addEventListener('click', ()=>{
-    window.location.href="https://www.agenciaminas.mg.gov.br/noticia/parque-eolico-volta-do-rio-passa-a-operar-com-totalidade-sob-gestao-da-cemig"
 })
